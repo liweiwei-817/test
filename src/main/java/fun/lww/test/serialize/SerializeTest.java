c1220440f143bee8926f0cd89ce678468acd9836
liweiwei <2622026762@qq.com>
2018-06-03 18:11:46 +0800

    20180603

--- src/main/java/fun/lww/test/serialize/SerializeTest.java
+++ src/main/java/fun/lww/test/serialize/SerializeTest.java
@@ -8,34 +8,29 @@ import java.io.*;
  */
 public class SerializeTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        try {
-            Person person = new Person();
-            person.setName("lww");
-            person.setAge(24);
-            person.setPhone(17611287817l);
+        Person person = new Person();
+        person.setName("lww");
+        person.setAge(24);
+        person.setPhone(17611287817l);
 
-            //序列化
-            FileOutputStream fileOutputStream = new FileOutputStream("C:/Users/liweiwei/Desktop/SerializeTest.ser");
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(person);
-            fileOutputStream.close();
-            objectOutputStream.close();
+        /*//序列化
+        FileOutputStream fileOutputStream = new FileOutputStream("C:/Users/liweiwei/Desktop/SerializeTest.ser");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(person);
+        fileOutputStream.close();
+        objectOutputStream.close();
 
-            //反序列化
-            FileInputStream fileInputStream = new FileInputStream("C:/Users/liweiwei/Desktop/SerializeTest.ser");
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            Person p = (Person) objectInputStream.readObject();
-            System.out.println(p);
-            fileInputStream.close();
-            objectInputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        //反序列化
+        FileInputStream fileInputStream = new FileInputStream("C:/Users/liweiwei/Desktop/SerializeTest.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Person p = (Person) objectInputStream.readObject();
+        System.out.println(p);
+        fileInputStream.close();
+        objectInputStream.close();*/
+
+        Person p = Util.clone(person);
+        System.out.println(p);
     }
 }
---
+++ src/main/java/fun/lww/test/serialize/Util.java
@@ -0,0 +1,32 @@
+package fun.lww.test.serialize;
+
+import java.io.*;
+
+/**
+ * 序列化
+ * Java 克隆对象两种方式
+ * 1. 实现Cloneable接口 重写clone方法
+ * 2. 实现Seriailzable接口 对象通过序列化 反序列化实现克隆 可以实现深度克隆
+ *
+ *
+ * 这里展示第二种
+ */
+public class Util {
+
+    public static  <T extends Serializable> T clone(T obj) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(obj);
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            return (T) objectInputStream.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
